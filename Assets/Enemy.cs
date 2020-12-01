13871af0f8df4efab7104680da6f8fe43c479eed
lucaforelli <62797944+lucaforelli@users.noreply.github.com>
2020-12-01 18:29:30 +0000

    adding explosion particles to enemies

---
+++ Assets/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //destroy an enemy if bird collides with it
+        if (collision.collider.GetComponent<Bird>() != null)
+        {
+            Destroy(gameObject);
+            return; //quit
+        }
+
+        //check if other objects collided with an enemy 
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            return; //quit 
+        }
+
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            Destroy(gameObject);
+            return; 
+        }
+    }
+}
