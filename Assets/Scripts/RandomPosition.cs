923ec3c739366d5ce9cc21152065509e6fb851d0
lucaforelli <62797944+lucaforelli@users.noreply.github.com>
2021-02-03 15:41:59 +0100

    Random Position Script
    moves box to 3 different positions at random

---
+++ Assets/Scripts/RandomPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    public Vector2[] positions;
+    void Start()
+    {
+        int randomNumber = Random.Range(0, positions.Length);
+        transform.position = positions[randomNumber];
+
+    }
+}
